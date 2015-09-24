d576b5b6d4b71b7aa907645336c34906ad891958
jblieden94 <jblieden@albany.edu>
2015-09-24 19:05:52 -0400

    Create css2.cs

---
+++ css/css2.cs
@@ -0,0 +1,57 @@
+.container{
+width: 1080px;
+clear: both;
+}
+.header h1{
+background-color: pink;
+float: left;
+margin: 0 50px 0 30px;
+clear: both;
+}
+nav ul li{
+display: inline;
+padding-left: 20px;
+margin-top: 10px;    
+}
+.content {
+float: left;
+background-color: #4CD871;
+width: 70%;
+}
+
+.top-content, img{
+float: left;
+clear: both;
+}
+
+.description{
+float: left;
+clear:right;
+}
+
+.aside {
+background-color: #FAEEB7;
+float: right;
+width: 30%;
+display: block;
+}
+
+.footer {
+background-color: #6078CF;
+clear:both;
+}
+.section1{
+display:inline;
+float: left;
+width:33%;
+}
+.section2{
+display:inline;
+float: left;
+width:33%;
+}
+.section3{
+display:inline;
+float: left;
+width:33%;
+}
